d0c61e4e80dceb94f861d1010d576c47d77a9ed5
Laura Roa <l.roa.2019@alumnos.urjc.es>
2022-03-08 19:29:25 +0100

    Intento de darle movimiento

--- src/CRobotMove.cpp
+++ src/CRobotMove.cpp
@@ -10,6 +10,7 @@ CRobotMove::CRobotMove(ros::NodeHandle& nh) :
 	mSpeed = nh.advertise<geometry_msgs::Twist> ("/cmd_vel", 1, true);
 	mSensorsSubs = nh.subscribe<visual_behavior_los_ultramarinos::RobotData>("/sensors_data", 1, &CRobotMove::OnRobotDataReceived, this);
 	mTimer = nh.createTimer(ros::Duration(0.1), &CRobotMove::spin, this);
+	//mTimer.stop();
 	mLinCtrler->SetTarget(1.f);
 
 }
@@ -18,22 +19,44 @@ CRobotMove::~CRobotMove(){
 
 }
 
-void CRobotMove::moveTo(double distancia, double angulo){
-	/*float theta = std::atan2(y - mSensorsData.position.y, x - mSensorsData.position.x) - mSensorsData.position.theta;
-	float x2 = x - mSensorsData.position.x;
-	float y2 = y - mSensorsData.position.y;
-	float dist = std::sqrt(x2*x2 + y2*y2);
-	std::cout << "Error Angulo: " << theta << ", dist: " << dist << std::endl;
-	//mIsInPosition = false;*/
+void CRobotMove::computeError(double& distancia, double& angulo){
+	visual_behavior_los_ultramarinos::PolarPoint pp_menor;
+	if (mSensorsData.boundingboxes.size() > 0){
+		pp_menor = mSensorsData.boundingboxes[0];
+	}
+	for(visual_behavior_los_ultramarinos::PolarPoint pp : mSensorsData.boundingboxes){
+		if (pp_menor.distancia > pp.distancia){
+			pp_menor = pp;
+		}
+	}
+	distancia = pp_menor.distancia;
+	angulo = pp_menor.angulo;
+}
+
+void CRobotMove::spin(const ros::TimerEvent& e){
+	ROS_INFO("UNA ITERACION DEL ROBOT");
+	double dist, ang;
+	computeError(dist, ang);
+	std::cout << "Error Angulo: " << ang << ", dist: " << dist << std::endl;
+	if(std::abs(ang) > 0.05){
+		float angSpeed;
+		mAngCtrler->GetSystemInput(ang, &angSpeed);
+		std::cout << "Velocidad angular: " << angSpeed << std::endl;
+		mSpeedCmd.linear.x = 0.f;
+		mSpeedCmd.linear.y = 0.f;
+		mSpeedCmd.angular.z = -angSpeed;
+	} else {
+		float linSpeed;
+		mLinCtrler->GetSystemInput(dist, &linSpeed);
+		std::cout << "Velocidad lineal: " << linSpeed << std::endl;
+		mSpeedCmd.linear.x = -linSpeed;
+		mSpeedCmd.linear.y = 0.f;
+		mSpeedCmd.angular.z = 0.f;
+	} 
+	mSpeed.publish(mSpeedCmd);
 }
 
 void CRobotMove::OnRobotDataReceived(const visual_behavior_los_ultramarinos::RobotDataConstPtr& msg)
 {
 	mSensorsData = *msg;
-}
-
-void CRobotMove::spin(const ros::TimerEvent& e){
-	if(not mIsInPosition){
-
-	}
 }
--- src/CSensors.cpp
+++ src/CSensors.cpp
@@ -47,7 +47,7 @@ void CSensors::OnBoundingBoxReceived(const darknet_ros_msgs::BoundingBoxesConstP
     		float len2 = std::abs(py - py_center);
     		float dot = px * px_center + py * py;
 
-    		float angle = std::acos(dot / (len1 * len2));
+    		float angle = 0.f; //std::acos(dot / (len1 * len2));
 
 			visual_behavior_los_ultramarinos::PolarPoint pp;
 			pp.distancia = dist;
@@ -59,4 +59,7 @@ void CSensors::OnBoundingBoxReceived(const darknet_ros_msgs::BoundingBoxesConstP
 			std::cout<<"HAY UNA PELOTA A " << dist << ", ÁNGULO: " << angle << "(" << angle*180.0/M_PI << ")" << std::endl;
 		}
 	}
+	mMsgSensorData.boundingboxes_size = mMsgSensorData.boundingboxes.size();
+	mSensorsPublisher.publish(mMsgSensorData);
+
 }
