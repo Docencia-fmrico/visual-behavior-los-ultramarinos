06162756a6d6cce9218b1c4babde753dcbb02ffd
Laura Roa <l.roa.2019@alumnos.urjc.es>
2022-03-08 18:21:38 +0100

    Modificación del Sensor

--- src/CRobotMove.cpp
+++ src/CRobotMove.cpp
@@ -3,14 +3,14 @@
 
 CRobotMove::CRobotMove(ros::NodeHandle& nh) : 
 	mNodeHandler(nh),
-	mLinCtrler(std::make_unique<CPidController>("Linear Speed Controller")),
-	mAngCtrler(std::make_unique<CPidController>("Angular Speed Controller")),
+	mLinCtrler(std::make_unique<CPidController>("Linear Speed Controller", 100, 0.4)),
+	mAngCtrler(std::make_unique<CPidController>("Angular Speed Controller", 100, 0.4)),
 	mIsInPosition(true)
 {
 	mSpeed = nh.advertise<geometry_msgs::Twist> ("/cmd_vel", 1, true);
 	mSensorsSubs = nh.subscribe<visual_behavior_los_ultramarinos::RobotData>("/sensors_data", 1, &CRobotMove::OnRobotDataReceived, this);
 	mTimer = nh.createTimer(ros::Duration(0.1), &CRobotMove::spin, this);
-
+	mLinCtrler->SetTarget(1.f);
 
 }
 
@@ -18,15 +18,13 @@ CRobotMove::~CRobotMove(){
 
 }
 
-void CRobotMove::moveTo(double x, double y){
-	mDestPose.x = x;
-	mDestPose.y = y;
-	float theta = std::atan2(y - mSensorsData.position.y, x - mSensorsData.position.x) - mSensorsData.position.theta;
+void CRobotMove::moveTo(double distancia, double angulo){
+	/*float theta = std::atan2(y - mSensorsData.position.y, x - mSensorsData.position.x) - mSensorsData.position.theta;
 	float x2 = x - mSensorsData.position.x;
 	float y2 = y - mSensorsData.position.y;
 	float dist = std::sqrt(x2*x2 + y2*y2);
 	std::cout << "Error Angulo: " << theta << ", dist: " << dist << std::endl;
-	//mIsInPosition = false;
+	//mIsInPosition = false;*/
 }
 
 void CRobotMove::OnRobotDataReceived(const visual_behavior_los_ultramarinos::RobotDataConstPtr& msg)
