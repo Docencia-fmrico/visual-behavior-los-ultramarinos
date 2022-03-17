3be967ff85c5ff84f314ceb14ce1738f6af12f06
Laura Roa <l.roa.2019@alumnos.urjc.es>
2022-03-17 19:44:52 +0100

    Roslint

--- include/visual_behavior/Controlador.hpp
+++ include/visual_behavior/Controlador.hpp
@@ -1,46 +1,50 @@
+// Copyright 2022 los ultramarinos
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef CONTROLADOR_HPP_
 #define CONTROLADOR_HPP_
 
-#include "stdio.h"
-#include <iostream>
-#include <vector>
 #include "visual_behavior/PIDController.hpp"
 #include "visual_behavior/Controlador.hpp"
 
+#include <stdio.h>
+#include <iostream>
+#include <vector>
 
-class Controlador {
-     
+class Controlador
+{
+public:
+  Controlador();
+  double errorGiro(double dg);
+  double errorAvance(double da);
+  double velocidadAngular(double errorAngular);
+  double velocidadLineal(double errorAvance);
 
- public:
-
-     Controlador();
-
-      double errorGiro( double dg) ;
-      double errorAvance( double da ) ;
-      double velocidadAngular( double errorAngular );
-      double velocidadLineal(  double errorAvance ) ;
-
-     
 protected:  
-     
-     
-     double distanciaMaxima ;
-     double distanciaSeguridad;
+  double distanciaMaxima;
+  double distanciaSeguridad;
 
+  std::vector<double> rangoAngular;
+  std::vector<double> rangoLineal;
 
-     std::vector<double> rangoAngular ;
-     std::vector<double> rangoLineal ;
+  std::vector<double> rangoAngularSalida;
+  std::vector<double> rangoLinealSalida;
 
-     
-     std::vector<double> rangoAngularSalida;
-     std::vector<double> rangoLinealSalida;
+  PIDController PIDAngular;
+  PIDController PIDLineal;
 
-     PIDController PIDAngular;
-     PIDController PIDLineal;
-
-     double alfa;
-} ;
-
+  double alfa;
+};
 
 #endif 
--- src/Control.cpp
+++ src/Control.cpp
@@ -1,3 +1,16 @@
+// Copyright 2022 los ultramarinos
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "ros/ros.h"
 #include "geometry_msgs/Pose2D.h"
@@ -7,62 +20,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double dg = 0 ;
-double da = 0 ;
-const int fr = 10 ; // frecuencia
+double dg = 0;
+double da = 0;
+const int fr = 10;  // frecuencia
 double v = 0;
 double w = 0;
 
 void messageCallback(const geometry_msgs::Pose2D::ConstPtr& msg)
 {
-  
-  dg = msg->y  ; //Bug en el pid de Paco si el rango de entrada es de -1 a 1 no te da los valores correctos en la salida xd
+  dg = msg->y;  // Bug en el pid, si el rango de entrada es de -1 a 1 no te da los valores correctos en la salida
   da = msg->x;
-
-  //ROS_INFO("Data: DG [%f]  ", dg);
-  //ROS_INFO("Data: DA [%f]  ", da);
-   
 }
 
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "Control");
 
-int main(int argc, char** argv){
-	ros::init(argc, argv, "Control");
+  ros::NodeHandle nh;
+  Controlador controlador = Controlador();
 
-	ros::NodeHandle nh;
-	Controlador controlador = Controlador();
+  ros::Subscriber sub = nh.subscribe("/controller_instructions", fr, messageCallback);
+  ros::Publisher pub_vel_ = nh.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity", fr);
 
-	ros::Subscriber sub = nh.subscribe("/controller_instructions", fr, messageCallback);
-	ros::Publisher pub_vel_ = nh.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity",fr);
+  geometry_msgs::Twist cmd;
+  ros::Rate loop_rate(fr);
 
+  while (ros::ok())
+  {
+    double errg = controlador.errorGiro(dg);
+    double erra = controlador.errorAvance(da);
 
-    geometry_msgs::Twist cmd;
-	ros::Rate loop_rate(fr);
-	
+    w = controlador.velocidadAngular(errg);
+    v = controlador.velocidadLineal(erra);
 
-	while(ros::ok()){
+    cmd.angular.z = w;
+    cmd.linear.x = v;
 
-		
-		double errg = controlador.errorGiro(dg) ; 
-        double erra = controlador.errorAvance(da) ;
-
-		w = controlador.velocidadAngular( errg );
-        v = controlador.velocidadLineal( erra );
-
-       
-	   //ROS_INFO("Data: v [%f]  ", v);
-  	   //ROS_INFO("Data: w [%f]  ", w);
-	   //ROS_INFO("Error distancia [%f]  ", erra );
-       //ROS_INFO("Data: DA [%f]  ", da);
-      
-
-	   cmd.angular.z = w;
-	   cmd.linear.x = v;
-
-	   pub_vel_.publish(cmd);
-	   ros::spinOnce();
-  
-	  
-	}
-			
-	return 0;
+    pub_vel_.publish(cmd);
+    ros::spinOnce();
+  }
+  return 0;
 }
